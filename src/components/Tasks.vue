29d621a74dc82d06de0b4f4e260d9b489058dcd4
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-07-03 15:31:14 -0400

    sections Dynamic Classes, Emit Events (Delete Tasks), Toggle Reminder, Add Task Component and v-model

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,19 +1,20 @@
 <template>
 	<div :key="task.id" v-for="task in tasks">
-		<Task :task="task" />
+		<Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task" />
 	</div>
 </template>
 
 <script>
-import Task from './Task'
+import Task from './Task';
 
 export default {
 	name: 'Tasks',
 	props: {
 		tasks: Array
+	},
+	components: {
+		Task
   },
-  components: {
-    Task
-  }
+  emits: ['delete-task', 'toggle-reminder']
 };
 </script>
